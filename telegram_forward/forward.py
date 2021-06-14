3340f1d3212550973017ec4659d73d465d6df50a
Valentin Bersier <vbersier@gmail.com>
2021-06-14 07:56:06 +0200

    Added some questionary stuff

--- telegram_forward/forward.py
+++ telegram_forward/forward.py
@@ -1,14 +1,18 @@
 import asyncio
 import logging
 import re
+import string
 import sys
 from collections import Counter
 from typing import List, Optional, Union
 
+import questionary
 from eth_typing.evm import ChecksumAddress
 from eth_utils.address import to_checksum_address
 from loguru import logger
+from questionary import ValidationError, Validator
 from telethon import TelegramClient, events
+from telethon.utils import parse_username
 
 
 class InterceptHandler(logging.Handler):
@@ -39,14 +43,10 @@ logger.add(
 )
 logging.basicConfig(handlers=[InterceptHandler()], level=0)
 
-tg_api_id: int = 0
-tg_api_hash: str = ''
-tg_channels: List[Union[int, str]] = []
-
 
 async def telegram_monitor(tg_api_id: int, tg_api_hash: str, tg_channels: List[Union[int, str]], forward_to):
     global logger
-    async with TelegramClient('pancakepump', tg_api_id, tg_api_hash) as client:
+    async with TelegramClient('telegram_forward', tg_api_id, tg_api_hash) as client:
         username = (await client.get_me()).username
         logger.info(f'Logged into Telegram as user {username}')
         logger.info('Monitoring channel for new messages...')
@@ -67,9 +67,49 @@ async def telegram_monitor(tg_api_id: int, tg_api_hash: str, tg_channels: List[U
         await client.run_until_disconnected()
 
 
+class IntegerValidator(Validator):
+    def validate(self, document):
+        if not str(document.text).isdigit():
+            raise ValidationError(message='Please enter a positive integer value')
+
+
+class TelegramApiHashValidator(Validator):
+    def validate(self, document):
+        if len(document.text) != 32 or not all(c in string.hexdigits for c in document.text):
+            raise ValidationError(message='Enter a valid Telegram hash (32 hexadecimal characters)')
+
+
+class TelegramUsernameOrLinkValidator(Validator):
+    def validate(self, document):
+        if 'joinchat' in document.text:
+            raise ValidationError(message='Invite links are not supported')
+
+
 def main():
+    # questionary stuff
+    api_id = questionary.text('Please provide your Telegram App api_id:', validate=IntegerValidator).unsafe_ask()
+    tg_api_hash = questionary.password(
+        'Please provide your secret Telegram App api_hash:', validate=TelegramApiHashValidator
+    ).unsafe_ask()
+    channels = questionary.text(
+        'Please provide the source channel(s) or chat(s) username(s) or ID(s)'
+        + ' (you can provide multiple values separated with a comma):',
+        validate=TelegramUsernameOrLinkValidator,
+    ).unsafe_ask()
+    channel_ids = [c.strip() for c in channels.split(',')]
+    tg_channels = []
+    for chan in channel_ids:
+        try:
+            cid = int(chan)
+        except ValueError:
+            cid, _ = parse_username(chan)
+        if cid is not None:
+            tg_channels.append(cid)
+    if not tg_channels:
+        logger.error('Could not extract a chat ID, aborting')
+        return
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(telegram_monitor())
+    loop.run_until_complete(telegram_monitor(tg_api_id=int(api_id), tg_api_hash=tg_api_hash, tg_channels=tg_channels))
 
 
 if __name__ == '__main__':
